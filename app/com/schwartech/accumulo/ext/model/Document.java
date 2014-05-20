768a881742be9869f03affb50f616a5edf90bb91
Jeffrey S. Schwartz <jeff@Jeffreys-MacBook-Pro.local>
2014-05-20 14:49:51 -0400

    Changed visibility of data to allow proper serialization

--- app/com/schwartech/accumulo/ext/model/Document.java
+++ app/com/schwartech/accumulo/ext/model/Document.java
@@ -1,5 +1,6 @@
 package com.schwartech.accumulo.ext.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Value;
 import org.apache.hadoop.io.Text;
@@ -16,11 +17,14 @@ public class Document {
 
     // TODO: JSS - I'm not sure if we need the original data
 //    private Map<Key, Value> originalData;
-    private Map<String, Value> indexData;
+
+    //TODO: JSS - Refactor this to be better for sending over the wire.
+    //TODO: JSS - Public may not be the best visibility
+    public Map<String, Value> data;
 
     public Document() {
 //        originalData = new HashMap<Key, Value>();
-        indexData = new HashMap<String, Value>();
+        data = new HashMap<String, Value>();
     }
 
     public void add(Key key, Value value) throws IllegalArgumentException {
@@ -31,19 +35,20 @@ public class Document {
         }
 
 //        originalData.put(key, value);
-        indexData.put(getIndexKey(key.getColumnFamily(), key.getColumnQualifier()), value);
+        data.put(getDataKey(key.getColumnFamily(), key.getColumnQualifier()), value);
     }
 
-    private String getIndexKey(String colFamily, String colQualifier) {
+    private String getDataKey(String colFamily, String colQualifier) {
         return colFamily + ":" + colQualifier;
     }
 
-    private String getIndexKey(Text colFamily, Text colQualifier) {
+    private String getDataKey(Text colFamily, Text colQualifier) {
         return colFamily + ":" + colQualifier;
     }
 
+    @JsonIgnore
     public Value getValue(String colFamily, String colQualifier) {
-        Value value = indexData.get(getIndexKey(colFamily, colQualifier));
+        Value value = data.get(getDataKey(colFamily, colQualifier));
 
         //TODO: JSS - this should return null as that is different than ""
         if (value == null) {
@@ -52,14 +57,17 @@ public class Document {
         return value;
     }
 
+    @JsonIgnore
     public Text getRowKey() {
         return this.rowKey;
     }
 
+    @JsonIgnore
     public Set<String> getKeys() {
-        return indexData.keySet();
+        return data.keySet();
     }
 
+    @JsonIgnore
     public String getValueAsString(String colFamilyQualifier) {
         int idx = colFamilyQualifier.indexOf(':');
         String fam = colFamilyQualifier.substring(0, idx);
@@ -67,6 +75,7 @@ public class Document {
         return getValueAsString(fam, qual);
     }
 
+    @JsonIgnore
     public String getValueAsString(String colFamily, String colQualifier) {
         return getValue(colFamily, colQualifier).toString();
     }
